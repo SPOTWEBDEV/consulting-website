78d5dd5958c0b6b88ba61475fe13df03fe1f75e5
somto ogbodo <somtoogbodo2007@gmail.com>
2025-04-23 14:16:38 +0100

    commit

---
+++ test.php
@@ -0,0 +1,23 @@
+<style>
+    .cer-work{
+        width:500px
+    }
+    .img-holder{
+        display:flex;
+        gap:20px;
+        flex-wrap:wrap
+    }
+    @media(max-width:700px){
+        .cer-work{
+        width:400px
+    }
+    }
+    @media(max-width:500px){
+        .cer-work{
+        width: 90%
+    }
+    }
+</style>
+<h3>About PIIB</h3><p>The Institute of Professional Independent Bookkeepers (PIIB) is a membership association for bookkeepers, designed to provide educational awareness to individuals or employees of business organizations who aspire to become professional bookkeepers. The PIIB equips individuals and groups with the knowledge needed to function as professional bookkeepers.</p><h3>PIIB Objectives</h3><p>The fundamental objective of PIIB is to provide training and enhance expertise, making members reliable, dependable, and accurate in preparing and reporting financial information. Members completing all four levels of training will be proficient in preparing and analyzing critical bookkeeping records like journal entries, ledger transactions, income statements, cash flow statements, and balance sheets.</p><h3>Learning Outline</h3><p>The certification program covers both foundational and advanced bookkeeping tasks. It consists of four levels of training, each building on the last. Subjects include:</p><h3>Levels</h3><ul><li>Level 1: Bookkeeping Orientation Level</li><li>Level 2: Analyzing and Recording Transactions</li><li>Level 3: Bookkeeping and Accounting Cycle Process</li><li>Level 4: Analysis of Financial Statement</li></ul><h3>Membership</h3><p>Before starting the training, candidates must register as a member of PIIB. Membership is essential for receiving professional skills and knowledge required in the bookkeeping career. There are two membership types: Student Member and Professional Member.</p><h3>Membership Types</h3><ul><li>Student Member: For beginners or students of any academic institution, with a non-refundable application fee of $30.99, and yearly dues of $39.99.</li><li>Professional Member: For individuals employed in the bookkeeping or accounting profession, with a non-refundable application fee of $49.99, and yearly dues of $59.99.</li></ul><h3>Ethical Considerations</h3><p>Members are expected to adhere to ethical standards in bookkeeping, including honesty, objectivity, confidentiality, and professionalism. These principles ensure trust in maintaining and securing client financial information.</p><h3>PIIB Services for Members</h3><ul><li>Level 1: Bookkeeping Orientation Level - $200</li><li>Level 2: Analyzing and Recording Transactions - $200</li><li>Level 3: Bookkeeping and Accounting Cycle Process - $200</li><li>Level 4: Analysis of Financial Statement - $200</li></ul><h3>Study Expectations</h3><p>All members must complete assignments and exams for each level within 2 weeks. Assistance is available for clarifying expectations. Upon completing the four levels and passing the exams, you will be awarded the Certified Independent Bookkeeper (CIB) designation.</p> <div class="img-holder">
+<img class="cer-work" src="https://bustaxbookkeeping.com/ceritificate/1.jpg" alt=""> <img src="https://bustaxbookkeeping.com/ceritificate/1.jpg"  class="cer-work" alt="">
+</div>
